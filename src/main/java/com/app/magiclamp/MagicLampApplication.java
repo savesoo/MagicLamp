4f4e0b69301de24930824822bb236ee2aa72141c
Seo-Aram <arams.office@gmail.com>
2023-01-03 15:41:50 +0900

    init setting

---
+++ src/main/java/com/app/magiclamp/MagicLampApplication.java
@@ -0,0 +1,13 @@
+package com.app.magiclamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicLampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicLampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/magiclamp/MagicLampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.magiclamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicLampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
