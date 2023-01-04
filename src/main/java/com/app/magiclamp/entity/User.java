0cb909a08b01b590abd5e7faed8e9562b581b6d4
Seo-Aram <arams.office@gmail.com>
2023-01-04 16:12:25 +0900

    엔티티 예제

---
+++ src/main/java/com/app/magiclamp/entity/User.java
@@ -0,0 +1,48 @@
+package com.app.magiclamp.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@Entity
+@DynamicInsert
+@Table(name="tbl_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer useridx;
+
+    @Column(columnDefinition = "varchar(20) unique not null")
+    private String username;
+    @Column(columnDefinition = "char(100) not null")
+    private String password;
+
+    @Column(columnDefinition = "varchar(20) not null")
+    private String name;
+    @Column(columnDefinition = "char(11) not null")
+    private String phone;
+
+    @Column(columnDefinition = "char(5) not null")
+    private String postnum;
+    @Column(columnDefinition = "varchar(200) not null")
+    private String address1;
+    @Column(columnDefinition = "varchar(100) not null")
+    private String address2;
+
+    @Column(columnDefinition = "timestamp not null default current_timestamp()")
+    private LocalDate joindate;
+    @Column(columnDefinition = "tinyint default 0")
+    private Boolean deleted;
+
+
+}
