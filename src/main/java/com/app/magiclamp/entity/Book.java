998b5377dba0232490858c242499be7fbf4c7790
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-05 00:15:53 +0900

    Book entity 주석 입력

--- src/main/java/com/app/magiclamp/entity/Book.java
+++ src/main/java/com/app/magiclamp/entity/Book.java
@@ -18,63 +18,63 @@ public class Book {
 
     @Id
     @Column
-    private String isbn;
+    private String isbn; // 도서ISBN
 
     @Column
-    private String title;
+    private String title; // 도서명
 
     @Column
-    private String author;
+    private String author; // 저자
 
     @Column
-    private String publisher;
+    private String publisher; // 출판사
 
     @Column
-    private Date pubdate;
+    private Date pubdate; // 출간일자
 
     @Column
-    private String status;
+    private String status; // 도서 상태 (정상, 절판, 품절)
 
     @Column
-    private Integer price;
+    private Integer price; // 정가
 
     @Column
-    private Integer saleprice;
+    private Integer saleprice; // 판매가(보통 10%할인)
 
     @Column
-    private Integer mileagerate;
+    private Integer mileagerate; // 마일리지적립률(보통 5% 적립)
 
     @Column
-    private Integer stock;
+    private Integer stock; // 재고 수량(시작 값은 10권)
 
     @Column
-    private String binding;
+    private String binding; // 도서 형태(PB or HB)
 
     @Column
-    private Integer pages;
+    private Integer pages; // 쪽수
 
     @Column
-    private String size;
+    private String size; // 사이즈 (가로mm, 세로mm)
 
     @Column
-    private Integer weight;
+    private Integer weight; // 무게 (기본 500g)
 
     @Column
-    private String category;
+    private String category; // 카테고리
 
     @Column
-    private String description;
+    private String description; // 도서 소개
 
     @Column
-    private String contents;
+    private String contents; // 도서 목차
 
     @Column
-    private String bookimg;
+    private String bookimg; // 도서 이미지
 
     @Column(columnDefinition = "timestamp not null default current_timestamp()")
-    private LocalDate regdate;
+    private LocalDate regdate; // 등록일자
 
     @Column
-    private LocalDate updatedate;
+    private LocalDate updatedate; // 수정일자
 
 }
