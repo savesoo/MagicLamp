799b43776072036701d8a2b668e69d1fe6488741
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 11:14:17 +0900

    Update BookMainListController.java

--- src/main/java/com/app/magiclamp/controller/book/BookMainListController.java
+++ src/main/java/com/app/magiclamp/controller/book/BookMainListController.java
@@ -1,4 +1,29 @@
 package com.app.magiclamp.controller.book;
 
+import com.app.magiclamp.service.book.BookMainListService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/view/book/bookList")
+@Log4j2
 public class BookMainListController {
+
+    @Autowired
+    private BookMainListService bookMainListService;
+
+    @GetMapping
+    public void getBookMainList(
+            @RequestParam(value = "p", defaultValue = "1") int pageNum,
+            Model model
+    ) {
+
+        model.addAttribute("BookMainListPage", bookMainListService.getBookMainPagelist(pageNum));
+
+    }
 }
