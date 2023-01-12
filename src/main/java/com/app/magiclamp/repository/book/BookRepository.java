0fd34fd20e1d371adba405c85d1c962fedb144e0
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 16:17:14 +0900

    Delete BookRepository.java

--- src/main/java/com/app/magiclamp/repository/book/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.magiclamp.repository.book;
-
-import com.app.magiclamp.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
