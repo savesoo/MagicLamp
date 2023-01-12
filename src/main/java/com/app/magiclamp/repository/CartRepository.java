712f15ea4e92f6a35745c51ab8ee43cc733865b7
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 17:54:08 +0900

    Update CartRepository.java

--- src/main/java/com/app/magiclamp/repository/CartRepository.java
+++ src/main/java/com/app/magiclamp/repository/CartRepository.java
@@ -2,6 +2,8 @@ package com.app.magiclamp.repository;
 
 import com.app.magiclamp.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
