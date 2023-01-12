7df7c6ec6cf2e71e62288a8c2385e5c84f5d8b1a
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 10:13:03 +0900

    Update BookViewService.java

--- src/main/java/com/app/magiclamp/service/book/BookViewService.java
+++ src/main/java/com/app/magiclamp/service/book/BookViewService.java
@@ -1,4 +1,20 @@
 package com.app.magiclamp.service.book;
 
+import com.app.magiclamp.entity.Book;
+import com.app.magiclamp.repository.book.BookRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
 public class BookViewService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book selectBook(String isbn) {
+
+        return bookRepository.findById(isbn).get();
+    }
 }
