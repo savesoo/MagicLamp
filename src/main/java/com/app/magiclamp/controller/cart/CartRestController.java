3bbbe9b5a91dd84ec5344b2552650e143a1e2a85
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 16:31:54 +0900

    Update CartRestController.java

--- src/main/java/com/app/magiclamp/controller/cart/CartRestController.java
+++ src/main/java/com/app/magiclamp/controller/cart/CartRestController.java
@@ -1,10 +1,13 @@
 package com.app.magiclamp.controller.cart;
 
+import com.app.magiclamp.entity.Cart;
 import com.app.magiclamp.repository.CartRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cart")
@@ -13,5 +16,15 @@ public class CartRestController {
 
     @Autowired
     private CartRepository cartRepository;
+
+    @PostMapping
+    public ResponseEntity<Cart> insertCart(
+            @RequestParam("list") List list
+    ) {
+
+        log.info("select list >>>> " + list);
+
+
+    }
 
 }
