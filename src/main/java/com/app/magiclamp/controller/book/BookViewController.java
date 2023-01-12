3c97bddfabe6f176d2be76514f1ab42edfb3e645
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 10:06:04 +0900

    도서상세페이지 컨트롤러 생성

--- src/main/java/com/app/magiclamp/controller/book/BookViewController.java
+++ src/main/java/com/app/magiclamp/controller/book/BookViewController.java
@@ -1,4 +1,38 @@
 package com.app.magiclamp.controller.book;
 
+import com.app.magiclamp.service.book.BookViewService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/book/view")
+@Log4j2
 public class BookViewController {
+
+    @Autowired
+    private BookViewService bookViewService;
+
+    @GetMapping
+    public void getBookView(
+            @RequestParam("isbn") String isbn,
+            @RequestParam("p") int pageNum,
+            Model model,
+            HttpServletRequest request
+    ) {
+
+//        HttpSession session = request.getSession();
+
+        model.addAttribute("pageNum", pageNum);
+        model.addAttribute("bookView", bookViewService.selectBook(isbn));
+
+    }
+
 }
