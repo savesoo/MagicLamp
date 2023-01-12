d177aa2ef3a816114fd6d993f1ec8eadf98336c9
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 17:44:41 +0900

    Update CartRestController.java

--- src/main/java/com/app/magiclamp/controller/cart/CartRestController.java
+++ src/main/java/com/app/magiclamp/controller/cart/CartRestController.java
@@ -1,28 +1,32 @@
 package com.app.magiclamp.controller.cart;
 
 import com.app.magiclamp.entity.Cart;
-import com.app.magiclamp.model.InsertSelCart;
-import com.app.magiclamp.repository.CartRepository;
+import com.app.magiclamp.model.InsertSelCartRequest;
+import com.app.magiclamp.service.cart.CartInsertService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cart")
 @Log4j2
 public class CartRestController {
 
-    @Autowired
-    private CartRepository cartRepository;
+    @Autowired(required = false)
+    private CartInsertService cartInsertService;
 
     @PostMapping
     public ResponseEntity<Cart> insertSelCart(
             @RequestBody InsertSelCartRequest insertSelCartRequest
     ) {
+
+        log.info("PostMapping 진입 insert 전 insertSelCartRequest ==> " + insertSelCartRequest);
+
+        Cart result = cartInsertService.insertSelCart(insertSelCartRequest);
 
 
 
