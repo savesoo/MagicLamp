83378e8d4275fac47788dd7e74d1f32ac54af48b
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-12 17:54:11 +0900

    Update CartInsertService.java

--- src/main/java/com/app/magiclamp/service/cart/CartInsertService.java
+++ src/main/java/com/app/magiclamp/service/cart/CartInsertService.java
@@ -3,8 +3,12 @@ package com.app.magiclamp.service.cart;
 import com.app.magiclamp.entity.Cart;
 import com.app.magiclamp.model.InsertSelCartRequest;
 import com.app.magiclamp.repository.CartRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Log4j2
 public class CartInsertService {
 
     @Autowired
