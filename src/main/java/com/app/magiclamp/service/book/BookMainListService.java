44f5945bd910ef68a1bf271d1b4b41afb9bd2233
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-16 11:25:40 +0900

    Update BookMainListService.java

--- src/main/java/com/app/magiclamp/service/book/BookMainListService.java
+++ src/main/java/com/app/magiclamp/service/book/BookMainListService.java
@@ -1,7 +1,9 @@
 package com.app.magiclamp.service.book;
 
 import com.app.magiclamp.entity.Book;
+import com.app.magiclamp.mapper.BookMainListMapper;
 import com.app.magiclamp.model.BookListPage;
+import com.app.magiclamp.model.BookSearchOption;
 import com.app.magiclamp.repository.BookRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +21,30 @@ public class BookMainListService {
     @Autowired
     private BookRepository bookRepository;
 
-    public BookListPage getBookMainPageList(int pagenum) {
+    @Autowired
+    private BookMainListMapper bookMainListMapper;
 
-        Page<Book> page = bookRepository.findAll(PageRequest.of(pagenum - 1, 10, Sort.by("pubdate").descending()));
+    public BookListPage getBookMainPageList(int pagenum, BookSearchOption searchOption) {
 
-        // 도서 리스트
-        List<Book> list = page.getContent();
+        BookListPage bookMainListPage = null;
 
-        // 전체 도서의 개수
-        int totalCount = (int) page.getTotalElements();
+        if (searchOption.getKeyword() == null || searchOption.getKeyword().length() <= 0){
 
-        BookListPage bookMainListPage = new BookListPage(10, pagenum, list, totalCount);
+            Page<Book> page = bookRepository.findAll(PageRequest.of(pagenum - 1, 10, Sort.by("pubdate").descending()));
+
+            // 도서 리스트
+            List<Book> list = page.getContent();
+
+            // 전체 도서의 개수
+            int totalCount = (int) page.getTotalElements();
+
+            bookMainListPage = new BookListPage(10, pagenum, list, totalCount);
+
+        } else {
+
+            bookMainListPage = bookMainListMapper.selectByOption(searchOption);
+
+        }
 
         return bookMainListPage;
     }
