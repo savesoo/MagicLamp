916bbf3849a2b1ef34ae259aa059d39b3b15783e
sangminby <113090480+sangminby@users.noreply.github.com>
2023-01-16 17:49:55 +0900

    AdministratorDelete

---
+++ src/main/java/com/app/magiclamp/controller/administrator/AdministratorDeleteController.java
@@ -0,0 +1,22 @@
+package com.app.magiclamp.controller.administrator;
+
+import com.app.magiclamp.service.administrator.AdministratorDeleteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdministratorDeleteController {
+
+    @Autowired
+    private AdministratorDeleteService administratorDeleteService;
+
+    @GetMapping("/view/administrator/delete")
+    public String deleteBook(@Param("isbn") String isbn) {
+
+        administratorDeleteService.deleteBook(isbn);
+
+        return "redirect:/view/administrator/list";
+    }
+}
