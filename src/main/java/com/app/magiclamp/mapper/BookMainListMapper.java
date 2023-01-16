7bc53cd96892fb5b823c183a0ba9f2663f1b6cb4
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-16 12:57:22 +0900

    Update BookMainListMapper.java

--- src/main/java/com/app/magiclamp/mapper/BookMainListMapper.java
+++ src/main/java/com/app/magiclamp/mapper/BookMainListMapper.java
@@ -3,11 +3,16 @@ package com.app.magiclamp.mapper;
 import com.app.magiclamp.entity.Book;
 import com.app.magiclamp.model.BookSearchOption;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Mapper
 public interface BookMainListMapper {
 
-    Page<Book> selectByOption(BookSearchOption searchOption);
+    List<Book> selectByOption(BookSearchOption searchOption);
+
+    int selectByOptionTotalCount(BookSearchOption searchOption);
 
 }
