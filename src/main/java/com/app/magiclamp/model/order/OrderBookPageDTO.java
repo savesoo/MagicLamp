53685385266a6dddf1698d002f712dbece3812ba
savesoo <hsw2657@gmail.com>
2023-01-16 19:33:46 +0900

    DTO 수정

---
+++ src/main/java/com/app/magiclamp/model/order/OrderBookPageDTO.java
@@ -0,0 +1,47 @@
+package com.app.magiclamp.model.order;
+
+import com.app.magiclamp.model.BookInfoDTO;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class OrderBookPageDTO {
+
+    // 사용자가 선택한 상품이 여러 개인 경우 주문 페이지에 출력 DTO
+
+    // 책 정보를 갖는 List
+    private List<BookInfoDTO> bookInfos;
+
+    // 이전 페이지(도서 상세)에서 가지고 올 data
+    private String isbn;
+    private int bookcount;
+
+    private Integer saleprice; // 판매가(보통 10%할인)
+
+
+    // 구매액과 마일리지
+    private int realprice;
+    private int mileage; // 현재 마일리지
+
+
+    // 배송정보
+    private String recipient;
+    private String phone;
+    private String postnum;
+    private String address1;
+    private String address2;
+
+
+    // DB에 없는 data
+    private int totalPrice; // 총 결제금액(최종 가격-realprice*수량)
+    private int saveMileage; // 권당 적립 마일리지
+    private int totalMileage; // 총 적립 마일리지(권당 적립 마일리지*수량)
+
+
+}
---
+++ src/main/java/com/app/magiclamp/model/order/RequestOrderBook.java
@@ -0,0 +1,21 @@
+package com.app.magiclamp.model.order;
+
+import com.app.magiclamp.model.BookInfoDTO;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class RequestOrderBook {
+
+    // 사용자가 선택한 상품 data를 주문 페이지로 보내줄 DTO (HTML에서 사용)
+
+    // 이전 페이지(도서 상세)에서 가지고 올 data        뷰->서버
+    private String isbn;
+    private int bookcount; // 책 수량
+
+
+}
