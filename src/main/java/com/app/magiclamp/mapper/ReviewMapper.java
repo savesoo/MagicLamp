852f6a2a40cfd8cebdb57d011b7d39f61bba91a2
himediahj <himedia.hj@gmail.com>
2023-01-17 20:50:54 +0900

    리뷰 매퍼 수정
    회원의 아이디 정보를 가져오기 위해 left join

--- src/main/java/com/app/magiclamp/mapper/ReviewMapper.java
+++ src/main/java/com/app/magiclamp/mapper/ReviewMapper.java
@@ -1,7 +1,11 @@
 package com.app.magiclamp.mapper;
 
+import com.app.magiclamp.model.ReviewDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ReviewMapper {
@@ -9,4 +13,8 @@ public interface ReviewMapper {
     // 평점
     @Select("select round(avg(star), 1) from tbl_review group by isbn having isbn=#{isbn}")
     Double calculateTheAverageOfStar(String isbn);
+
+    // 리뷰 목록
+    @Select("select * from tbl_review as r left outer join tbl_user as u on r.reviewer=u.userindex where r.isbn=#{isbn} order by r.reviewindex desc limit #{start}, #{end}")
+    List<ReviewDTO> getList(@Param("isbn") String isbn, @Param("start") int start, @Param("end") int end);
 }
