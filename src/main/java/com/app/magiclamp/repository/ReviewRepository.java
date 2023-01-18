d1a0ed2909f5ec984d7b832dd4e3ee3932b2d47c
himediahj <himedia.hj@gmail.com>
2023-01-18 10:22:32 +0900

    수정

--- src/main/java/com/app/magiclamp/repository/ReviewRepository.java
+++ src/main/java/com/app/magiclamp/repository/ReviewRepository.java
@@ -3,17 +3,19 @@ package com.app.magiclamp.repository;
 import com.app.magiclamp.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-
-    // 책 별로 리뷰 리스트 (최신 등록순)
-    List<Review> findByIsbn_IsbnOrderByRegdateDesc(String isbn);
 
     // 각 별점을 매긴 사람 수
     long countByIsbn_IsbnAndStar(String isbn, Integer star);
 
     // 총 별점을 매긴 사람 수
     long countByIsbn_Isbn(String isbn);
+
+    // 본인이 쓴 리뷰 정보
+    Optional<Review> findByIsbn_IsbnAndReviewer_Userindex(String isbn, Integer userindex);
+
+
 
 }
