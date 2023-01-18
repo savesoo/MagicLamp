4c5520e8a1bf9ecc3d2a9bc8e8c550109744f435
himediahj <himedia.hj@gmail.com>
2023-01-18 09:15:24 +0900

    리뷰 처리

--- src/main/java/com/app/magiclamp/controller/book/BookViewController.java
+++ src/main/java/com/app/magiclamp/controller/book/BookViewController.java
@@ -1,8 +1,11 @@
 package com.app.magiclamp.controller.book;
 
+import com.app.magiclamp.model.AuthUserDTO;
+import com.app.magiclamp.service.review.ReviewReadService;
 import com.app.magiclamp.service.book.BookViewService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,21 +22,34 @@ public class BookViewController {
     @Autowired
     private BookViewService bookViewService;
 
+    @Autowired
+    private ReviewReadService reviewReadService;
+
     @GetMapping
     public void getBookView(
-           //@RequestParam("isbn") String isbn,
-            //@RequestParam("p") int pageNum,
+            @RequestParam("isbn") String isbn,
+            @RequestParam("p") int pageNum,
             Model model,
-            HttpServletRequest request
-    ) {
+            HttpServletRequest request,
+            @AuthenticationPrincipal AuthUserDTO authUserDTO
+            ) {
 
 //        HttpSession session = request.getSession();
 
-        String isbn = "9788925578415";
-        int pageNum = 1;
+        /*String isbn = "9788925578415";
+        int pageNum = 1;*/
 
         model.addAttribute("pageNum", pageNum);
         model.addAttribute("bookView", bookViewService.selectBook(isbn));
+
+        model.addAttribute("avg", reviewReadService.calculateTheAvg(isbn));
+        model.addAttribute("people", reviewReadService.countPeople(isbn));
+        model.addAttribute("star5", reviewReadService.count5(isbn));
+        model.addAttribute("star4", reviewReadService.count4(isbn));
+        model.addAttribute("star3", reviewReadService.count3(isbn));
+        model.addAttribute("star2", reviewReadService.count2(isbn));
+        model.addAttribute("star1", reviewReadService.count1(isbn));
+        model.addAttribute("loginInfo", authUserDTO == null? -1 : authUserDTO.getUserindex());
 
         log.info("bookViewService.selectBook(isbn) >>>>>>>>>>>>>>>>> " + bookViewService.selectBook(isbn));
 
