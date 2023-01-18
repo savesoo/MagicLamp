1f7d7b6854f8e60ec99a186cf0f974e0e7532c57
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-18 09:19:19 +0900

    Update CartInsertService.java

--- src/main/java/com/app/magiclamp/service/cart/CartInsertService.java
+++ src/main/java/com/app/magiclamp/service/cart/CartInsertService.java
@@ -1,11 +1,14 @@
 package com.app.magiclamp.service.cart;
 
 import com.app.magiclamp.entity.Cart;
+import com.app.magiclamp.mapper.CartMapper;
 import com.app.magiclamp.model.InsertSelCartRequest;
 import com.app.magiclamp.repository.CartRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -14,15 +17,43 @@ public class CartInsertService {
     @Autowired
     private CartRepository cartRepository;
 
-    public Cart insertSelCart(InsertSelCartRequest insertSelCartRequest) {
-               
+    @Autowired
+    private CartMapper cartMapper;
+
+    public Cart insertSelCart(List<InsertSelCartRequest> insertSelCartRequest) {
+
+        log.info("insertSelCartRequest................. ==> " + insertSelCartRequest);
+
+        Cart result = null;
+
+        for (InsertSelCartRequest istReq : insertSelCartRequest) {
+
+            String isbnCart = istReq.getIsbn();
+            Integer userindexCart = istReq.getUserindex();
+
+            Cart searchResult = cartMapper.searchIsbn(isbnCart, userindexCart);
+
+            if (searchResult == null) {
+
+                Cart cart = istReq.toCart();
+
+                log.info("CartInsert 진입 전 cart  ==> " + cart);
+
+                result = cartRepository.save(cart);
+
+                log.info("CartInsert 진입 후 result ==> " + result);
+
+            }
+        }
+
+        return result;
+    }
+
+    public Cart insertOneCart(InsertSelCartRequest insertSelCartRequest) {
+
         Cart cart = insertSelCartRequest.toCart();
 
-        log.info("CartInsert 진입 전 cart  ==> " + cart);
-
         Cart result = cartRepository.save(cart);
-
-        log.info("CartInsert 진입 후 result ==> " + result);
 
         return result;
     }
