2c718975f7911ee0b97a066a10f3c66d0788963f
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-19 13:18:16 +0900

    Update BookMainListMapper.java

--- src/main/java/com/app/magiclamp/mapper/BookMainListMapper.java
+++ src/main/java/com/app/magiclamp/mapper/BookMainListMapper.java
@@ -1,22 +1,19 @@
 package com.app.magiclamp.mapper;
 
-import com.app.magiclamp.entity.Book;
 import com.app.magiclamp.model.bookMain.BookRequest;
 import com.app.magiclamp.model.bookMain.BookSearchOption;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 @Mapper
 public interface BookMainListMapper {
 
+    //
     List<BookRequest> selectByOption(@Param("p1") int stnum, @Param("p2") BookSearchOption searchOption);
 
+    // 전체 수량
     int selectByOptionTotalCount(BookSearchOption searchOption);
 
-    @Select("select r.star, b.* from tbl_book b left outer join tbl_review r on b.isbn=r.isbn order by pubdate desc limit #{param1}, #{param2}")
-    List<BookRequest> selectBookStar(int i, int i1);
 }
