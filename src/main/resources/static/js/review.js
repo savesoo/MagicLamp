893abc2fdfb7fd563566e9a437973d7f482ad9d5
himediahj <himedia.hj@gmail.com>
2023-01-27 17:15:51 +0900

    계속 읽기 오류 수정
    페이지 처리할 때마다 반복처리 때문에 toggle_content 함수가 새로 정의되어서 함수를 따로 뺐습니다

--- src/main/resources/static/js/review.js
+++ src/main/resources/static/js/review.js
@@ -18,7 +18,7 @@ function loginCheck() {
         }
     } else {
         promise.then(result => {
-            if(result == 0) alert('책을 구매한 회원만 리뷰 등록이 가능합니다.')
+            if (result == 0) alert('책을 구매한 회원만 리뷰 등록이 가능합니다.')
         })
     }
 }
@@ -32,7 +32,7 @@ function loginCheckCh() {
     }
 }
 
-function orderCheck(){
+function orderCheck() {
     return axios.get('/review/register/check/' + isbn + '/' + loginInfo).then(response => response.data)
 }
 
@@ -141,6 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const review_btn = document.querySelector('.review_btn')
 
     // 버튼 이벤트 등록
+    review_btn.addEventListener('click', loginCheck)
     review_btn.addEventListener('click', insertReview)
 
     setReviewList(1)
@@ -186,51 +187,21 @@ function makeList(respond, page) {
                 html += '<p class="StarRate_IconFill" style="width: 100%"></p>'
                 break
         }
+        if (review.username.length > 3) review.username = review.username.substring(0, 3) + '***'
         html += '</div><div class="reviewer_info">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <span>' + review.username + '</span></div></div>'
         html += '<div class="review_date">' + review.regdate[0] + '.' + review.regdate[1] + '.' + review.regdate[2] + '</div></div>'
-        html += '<div class="list_right"><p class="review_content">' + review.reviewcontent + '</p>'
+
+        if (review.reviewcontent.length >= 140) {
+            html += '<div class="list_right"><p class="review_content"><span class="visible">' + review.reviewcontent.substring(0, 140) + "..." + '</span><button class="more_review" onclick="toggle_content()">계속 읽기</button><span class="hidden">' + review.reviewcontent + '</span></p>'
+        } else html += '<div class="list_right"><p class="review_content">' + review.reviewcontent + '</p>'
+
         html += '<div class="review_status"><button type="button" class="like_button" onclick="likeButton()">'
-        html += '<span class="button_icon"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="15" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">\n' +
-            '  <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>\n' +
-            '</svg></span><span class="like_text">' + review.cnt + '</span></button></div></div>'
+        html += '<span class="button_icon"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="15" fill="currentColor" class="bi bi-hand-thumbs-up-fill" viewBox="0 0 16 16">\n' + '  <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z"/>\n' + '</svg></span><span class="like_text">' + review.cnt + '</span></button></div></div>'
 
         newLI.innerHTML = html
         newLI.setAttribute('index', review.reviewindex)
         reviewList.appendChild(newLI)
     });
-
-    const sections = document.querySelectorAll('.review_content')
-    for (i = 0; i < sections.length; i++) {
-        const item = sections.item(i);
-
-        const content_text = item.textContent
-        const content_text_short = content_text.substring(0, 140) + "..."
-        const btn_more = document.createElement('button')
-        btn_more.setAttribute('class', 'more_review')
-        btn_more.textContent = '계속 읽기'
-
-        const visible_content = document.createElement('span')
-        visible_content.setAttribute('class', 'visible')
-        visible_content.textContent = content_text_short
-
-        const hidden_content = document.createElement('span')
-        hidden_content.setAttribute('class', 'hidden')
-        hidden_content.textContent = content_text
-
-        if (content_text.length >= 140) {
-            item.innerHTML = ''
-            item.appendChild(visible_content)
-            visible_content.appendChild(btn_more)
-            item.appendChild(hidden_content)
-        }
-
-        btn_more.addEventListener("click", toggle_content)
-
-        function toggle_content() {
-            item.firstChild.className = 'hidden'
-            item.lastChild.classList.remove('hidden')
-        }
-    }
 
     const thumbs = document.querySelectorAll('.like_button')
     for (i = 0; i < thumbs.length; i++) {
@@ -255,6 +226,15 @@ function setReviewList(pageNum) {
         }).catch()
 }
 
+function toggle_content() {
+    const parent = event.currentTarget.parentElement
+    const visible = event.currentTarget.parentElement.children[0]
+    const hidden = event.currentTarget.parentElement.children[2]
+    visible.classList.value = 'hidden'
+    parent.removeChild(parent.children[1])
+    hidden.classList.remove('hidden')
+}
+
 function likeButton() {
 
     if (loginInfo < 0) {
@@ -267,8 +247,7 @@ function likeButton() {
     const id = parentTag.getAttribute('index')
 
     const payload = {
-        reviewindex: id,
-        userindex: document.querySelector('#reviewer').value
+        reviewindex: id, userindex: document.querySelector('#reviewer').value
     }
     if (event.currentTarget.getAttribute('class') == 'like_button') {
         axios.post('/like/' + id, payload)
@@ -288,29 +267,30 @@ function likeButton() {
 
 function insertReview() {
 
-    if (starFieldset.getAttribute('check') != 'checked') {
-        alert('별점을 먼저 남겨주세요.')
-    }
+    if (loginInfo > 0) {
+        if (starFieldset.getAttribute('check') != 'checked') {
+            alert('별점을 먼저 남겨주세요.')
+        }
 
-    const payload = {
-        isbn: isbn,
-        reviewer: document.querySelector('#reviewer').value,
-        star: document.querySelector('input[name="star"]:checked').value,
-        reviewcontent: document.querySelector('.review_input_textarea').value
-    }
-    console.log("payload", payload)
+        const payload = {
+            isbn: isbn,
+            reviewer: document.querySelector('#reviewer').value,
+            star: document.querySelector('input[name="star"]:checked').value,
+            reviewcontent: document.querySelector('.review_input_textarea').value
+        }
+        console.log("payload", payload)
 
-    if (payload.reviewcontent == '') {
-        alert('내용을 작성해주세요.')
-    } else {
-        axios.post('/review', payload)
-            .then(res => {
-                console.log('post -> response', res.data)
-                location.reload()
-            })
-            .catch(err => console.log(err))
+        if (payload.reviewcontent == '') {
+            alert('내용을 작성해주세요.')
+        } else {
+            axios.post('/review', payload)
+                .then(res => {
+                    console.log('post -> response', res.data)
+                    location.reload()
+                })
+                .catch(err => console.log(err))
+        }
     }
-
 }
 
 function deleteReview(reviewindex) {
