c9ce9756e942eee5e578292c640faee095fc331f
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-27 16:10:53 +0900

    Update IndexController.java

--- src/main/java/com/app/magiclamp/controller/IndexController.java
+++ src/main/java/com/app/magiclamp/controller/IndexController.java
@@ -1,18 +1,27 @@
 package com.app.magiclamp.controller;
 
+import com.app.magiclamp.model.AuthUserDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index(HttpServletRequest request, HttpServletResponse response) {
+    public String index(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @AuthenticationPrincipal AuthUserDTO authUserDTO,
+            Model model
+    ) {
 
         Cookie[] cookies = request.getCookies();
 
@@ -24,6 +33,8 @@ public class IndexController {
                 }
             }
         }
+
+        model.addAttribute("loginInfo", authUserDTO == null ? -1 : authUserDTO.getUserindex());
 
         return "view/index";
     }
