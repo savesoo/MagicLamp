69c65bd55d24d5533def73e396c7cc6a55a0ac73
skjung1979 <skjung1979.dvlp@gmail.com>
2023-01-27 15:12:49 +0900

    Update IndexController.java

--- src/main/java/com/app/magiclamp/controller/IndexController.java
+++ src/main/java/com/app/magiclamp/controller/IndexController.java
@@ -4,11 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String index() {
+    public String index(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("prePath")) {
+                    c.setMaxAge(0);
+                    response.addCookie(c);
+                }
+            }
+        }
+
         return "view/index";
     }
 }
