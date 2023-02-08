962c1dad8aea2239a643d85e998de18b39c4ef84
sangminby <113090480+sangminby@users.noreply.github.com>
2023-02-08 16:12:17 +0900

    administratorModifyController

--- src/main/java/com/app/magiclamp/controller/administrator/AdministratorModifyController.java
+++ src/main/java/com/app/magiclamp/controller/administrator/AdministratorModifyController.java
@@ -7,11 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/administrator/modify")
@@ -22,7 +20,6 @@ public class AdministratorModifyController {
 
     @Autowired
     private AdministratorModifyService administratorModifyService;
-
 
     @GetMapping
     public String getModify(@RequestParam("isbn") String isbn, Model model) {
