3aee7d46bd3d603c1ae2fe872674f24298286212
savesoo <hsw2657@gmail.com>
2023-02-09 13:11:12 +0900

    수정시 파일 그대로 올라가기
    .

--- src/main/java/com/app/magiclamp/service/administrator/AdministratorModifyService.java
+++ src/main/java/com/app/magiclamp/service/administrator/AdministratorModifyService.java
@@ -10,15 +10,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
 
 @Service
+@Log4j2
 public class AdministratorModifyService {
 
     @Autowired
     private BookAdministratorRepository bookAdministratorRepository;
 
     public int modify(BookModifyRequest bookModifyRequest) {
+
+        log.info(" >>>>>>>> modify service >>>>>>>> ");
 
         MultipartFile multipartFile = bookModifyRequest.getBookimg();
 
@@ -27,22 +29,24 @@ public class AdministratorModifyService {
 
         if(multipartFile != null && !multipartFile.isEmpty()) {
 
+            // 새 이미지 저장
             String absolutePath = new File("").getAbsolutePath();
 
             String path = "photo";
             saveDir = new File(absolutePath, path);
 
+            // 폴더 없으면 생성
             if(!saveDir.exists()) {
                 saveDir.mkdirs();
             }
 
+            // 새 파일 이름 + 저장 경로
             newFileName = bookModifyRequest.getIsbn()+".jpg";
             File newFile = new File(saveDir, newFileName);
 
             try {
-                if(newFileName!=null && !newFileName.isEmpty()) {
-                    multipartFile.transferTo(newFile);
-                }
+                // 파일 저장
+                multipartFile.transferTo(newFile);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -51,35 +55,37 @@ public class AdministratorModifyService {
         }
 
         Book book = bookModifyRequest.toBookEntity();
-
-
+        
         if(newFileName != null && !newFileName.isEmpty()) {
             book.setBookimg(newFileName);
-
-        }
-        else {
-            book.setBookimg("");
         }
 
         int result = 0;
 
         try {
 
-            bookAdministratorRepository.save(book);
+            // DB에 넣어주기
+            result = bookAdministratorRepository.save(book) !=null ? 1 : 0;
 
+            log.info(" >>>>> modify result >>>>> " + result);
+
+            // 새 파일 저장하는데 이전 파일 존재시 삭제(=새 이미지로 바꿔서 저장)
             String oldFileName = bookModifyRequest.getOldimg();
-
             if(newFileName != null && !newFileName.isEmpty() && oldFileName != null && !oldFileName.isEmpty()) {
-                File delOldFile = new File(saveDir, oldFileName);
+
+                File delOldFile = new File(saveDir, oldFileName); // 삭제 경로
 
                 if(delOldFile.exists()) {
-                    delOldFile.delete();
+                    delOldFile.delete(); // 삭제 처리
+                    log.info(" >>>>>>>> deleted oldfile >>>>>>>> " + delOldFile.exists());
                 }
             }
 
         } catch (Exception e) {
 
             if(newFileName != null && !newFileName.isEmpty()) {
+
+                // 새 파일 삭제
                 File delFile = new File(saveDir, newFileName);
 
                 if(delFile.exists()) {
